3b406c3d2a6c2aea1fdcdc2b58fa5841d867f204
Turing machine <igor@Turings-Air.digi.box>
2022-11-24 04:13:32 +0100

    Refactored and extracted constants from ForecastTable into constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,11 @@ const CONSTANTS = {
         CELCIUS_SYMBOL: '\u00b0C',
         FAHRENHEIT_SYMBOL: '\u00b0F',
         WEEK_DAYS: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        MONTHS: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        MONTHS: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        FORECAST_TIMESTAMPS_NUMBER: 5,    // defines how many columns there wil be in the ForecastTable
+        MS_IN_A_SECOND: 1000,
+        WEATHER_ICON_URL_START: "https://openweathermap.org/img/wn/",
+        MOBILE_MAX_WIDTH: 990,
 }
 
 export default CONSTANTS
