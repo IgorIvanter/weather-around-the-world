ce99e7fe0e9da5f47f0705533c0164005fd827fa
Turing machine <igor@Turings-Air.digi.box>
2022-11-24 03:46:48 +0100

    Refactored...

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,10 @@ import ForecastTable from "./ForecastTable"
 import CONSTANTS from "../constants"
 
 
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const DateBox = () => {
 	const date = new Date()
 	return <div className="date-box">
@@ -10,36 +14,28 @@ const DateBox = () => {
 }
 
 const Main = ({state}) => {
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-	/* Conditional Rendering: if the response to the initial request didn't come, <h1>Fetching data...<h1> is displayed */
+    // Conditional Rendering: if the response to the initial request didn't come, <h1>Fetching data...<h1> is displayed */
 
 	// TODO: for that, use a bool flag that will be stored in the state, false when data didn't come
 
-	return Object.keys(state).length > 1 ? (
-		<div>
+	if (Object.keys(state).length > 1) {
+		return (
 			<main>
 				<div className="location-box text-center text">
 					{state.location}
 					<DateBox />
 				</div>
-				<div className="temp-box">
-					{Math.round(state.temp)} {CONSTANTS.CELCIUS_SYMBOL}
+				<div className="temp-box text">
+					{Math.round(state.temp)}{CONSTANTS.CELCIUS_SYMBOL}
 				</div>
 				<div className="description-box text-center text">
 					Feels like {Math.round(state.feelsLike)}{CONSTANTS.CELCIUS_SYMBOL}. {capitalizeFirstLetter(state.description)}.
 				</div>
 				<ForecastTable state={state} />
-			</main>
-		</div>)
-		: 
-		(
-			<main>
-				<h1>Fetching data...</h1>
-			</main>
-		)
+			</main>)
+	} else {
+		return (<main><h1>Fetching data...</h1></main>)
+	}
 }
 
 export default Main
